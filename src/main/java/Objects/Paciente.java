be1c4a0957431cdc27fd9532413c45c791a2cbab
daniv <laurad.vegah@uqvirtual.edu.co>
2025-11-18 17:11:56 -0500

    lol

--- src/main/java/Objects/Paciente.java
+++ src/main/java/Objects/Paciente.java
@@ -1,4 +1,58 @@
-package Objects;
+package objects;
 
 public class Paciente {
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Cliente(String id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = this.id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Objects.Paciente{");
+        sb.append("id='").append(getId()).append('\'');
+        sb.append(", name='").append(getName()).append('\'');
+        sb.append(", phone='").append(getPhone()).append('\'');
+        sb.append(", email='").append(getEmail()).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
